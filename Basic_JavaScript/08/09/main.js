090f0bdc1e5ce5818085d47904a8a9ddca829c78
takyu <takyu613.study@gmail.com>
2022-02-02 16:21:45 +0900

    MacroTasks and MicroTasks Part2

---
+++ Basic_JavaScript/08/09/main.js
@@ -0,0 +1,37 @@
+new Promise(function promise(resolve) {
+	console.log('promise');
+
+	setTimeout(function task1() {
+		console.log('task1');
+		resolve();
+	});
+
+}).then(function job1() {
+	console.log('job1');
+
+	setTimeout(function task2() {
+		console.log('task2');
+	
+		queueMicrotask(function job5() {
+			console.log('job5');
+		});
+
+		// queueMicrotaskのPromiseメソッドを使った書き換え
+		const p = Promise.resolve();
+		p.then(function job5() {
+			console.log('job5');
+		});
+
+	});
+
+	queueMicrotask(function job4() {
+		console.log('job4');
+	});
+
+}).then(function job2() {
+	console.log('job2');
+}).then(function job3() {
+	console.log('job3');
+});
+
+console.log('global end');
