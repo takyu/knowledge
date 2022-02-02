ca7ff5300167a90563969936dd0eb1bd2b6af231
takyu <takyu613.study@gmail.com>
2022-02-02 18:18:32 +0900

    Mobile Menu written based on BEM (Implement to design the menu)

---
+++ Practical_JS/01/07/js/main.js
@@ -0,0 +1,31 @@
+import { nodeOps as ns } from './nodeOps.js';
+
+class MobileMenu {
+	constructor() {
+		this.DOM = {};
+		this.DOM.btn = ns.qs('.mobile-menu__btn');
+		this.DOM.cover = ns.qs('.mobile-menu__cover');
+		this.DOM.container = ns.qs('#global-container');
+		this.eventType = this._getEventType();
+		this._addEvent();
+	}
+
+	_getEventType() {
+		if (Modernizr.touchevents) {
+			return "touchstart";
+		} else {
+			return "click";
+		}
+	}
+
+	_toggle() {
+		ns.cltt(this.DOM.container, 'menu-open');
+	}
+
+	_addEvent() {
+		ns.on(this.DOM.btn, this.eventType, this._toggle.bind(this));
+		ns.on(this.DOM.cover, this.eventType, this._toggle.bind(this));
+	}
+}
+
+new MobileMenu();
