0dee9f3c94244dd0be892abfbca79872b0bb35bc
takyu <takyu613.study@gmail.com>
2022-02-02 18:20:34 +0900

    Hero Slider (text animation)

---
+++ Practical_JS/01/12/js/main.js
@@ -0,0 +1,9 @@
+import Swiper from '../../node_modules/swiper/swiper-bundle.esm.browser.min.js';
+
+const swiper = new Swiper('.swiper', {
+	// Optional parameters
+	//direction: 'vertical',
+	loop: true,
+	//effect: 'fade',
+	effect: 'coverflow',
+});
