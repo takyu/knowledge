bf6f2209a254caf85ad8ac1b130255afb48e1faf
takyu <takyu613.study@gmail.com>
2022-02-02 18:19:59 +0900

    Tutorial of Swiper (use Swiper)

---
+++ Practical_JS/01/10/js/main.js
@@ -0,0 +1,25 @@
+import Swiper from '../../node_modules/swiper/swiper-bundle.esm.browser.min.js';
+
+const swiper = new Swiper('.swiper', {
+	// Optional parameters
+	//direction: 'vertical',
+	loop: true,
+	//effect: 'fade',
+	effect: 'coverflow',
+  
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination',
+	},
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+
+	// And if we need scrollbar
+	scrollbar: {
+		el: '.swiper-scrollbar',
+	},
+});
