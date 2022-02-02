13178c30af1226cf20db61fb838a492a3cab2e0a
takyu <takyu613.study@gmail.com>
2022-02-02 17:11:53 +0900

    DOM (Part.1)

---
+++ Basic_JavaScript/11/01/main.js
@@ -0,0 +1,11 @@
+// body取得
+const body = document.querySelector('body');
+console.dir(body);
+
+// button取得
+const button = document.querySelector('#inc');
+console.dir(button);
+
+// 探す範囲を限定的に
+const button2 = body.querySelector('#inc');
+console.dir(button2);
