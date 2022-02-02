901f062e78b4be57f1fd4ecb6a8ea6193884eeee
takyu <takyu613.study@gmail.com>
2022-02-02 14:47:42 +0900

    let, const, and var

---
+++ Basic_JavaScript/04/01/main.js
@@ -0,0 +1,31 @@
+let a = 1;
+// let a = 1;
+
+var b = 1;
+var b = 2;
+
+console.log(b);
+
+let c = 3;
+c = 4;
+
+console.log(c);
+
+const d = 1;
+// d = 2;
+
+console.log(f);
+
+{
+	let e = 0;
+	var f = 0;
+}
+
+// console.log(e);
+
+// ホイスティングによる違い
+console.log(g);
+console.log(h);
+
+var g = 7;
+let h = 9;
