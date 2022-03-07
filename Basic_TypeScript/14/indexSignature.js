8bdbadb77d5c4e3652c774d9d81af1d26c98247c
takyu <takyu613.study@gmail.com>
2022-03-07 21:49:39 +0900

    index signature

---
+++ Basic_TypeScript/14/indexSignature.js
@@ -0,0 +1,35 @@
+/**
+ * Index Signature
+ *
+ * interfaceやtype aliasでオブジェクトを定義する際に
+ * オブジェクトに含まれるプロパティ名を設定できる
+ *
+ * [key: string]
+ * -> keyには任意の名前をつけることができ、その名前はstringになることを表す
+ */
+{
+    var user = {
+        firstName: 'John',
+        lastName: 'Doe'
+    };
+    // 追加
+    user.middleName = 'T';
+    user['gender'] = 'male';
+    var user2 = {
+        1: 'John',
+        10: 'T',
+        100: 'Doe'
+    };
+    console.log('user2[1]: ' + user2[1]);
+    console.log('user2[10]: ' + user2[10]);
+    console.log('user2[100]: ' + user2[100]);
+    var pInfo = {
+        firstName: "John",
+        lastName: "Doe",
+        middleName: "M",
+        age: 10,
+        studentNum: 30
+    };
+    console.log('pInfo:');
+    console.log(pInfo);
+}
