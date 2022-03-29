d9d803e06f00c94759ae13b592065f77055774e1
takyu <takyu613.study@gmail.com>
2022-03-29 21:28:58 +0900

    add 03_interface

--- Basic_TypeScript/03/interface.js
+++ Basic_TypeScript/03/interface.js
@@ -15,16 +15,24 @@
         return "hello, ".concat(p.firstName, " ").concat(p.lastName);
     };
     var user = {
-        firstName: "Jane",
-        lastName: "Doe"
+        firstName: 'Jane',
+        lastName: 'Doe',
+        // Personインタフェースにプロパティが記入されていないためエラー
+        // age: 35
     };
     console.log(greeter(user));
     var user2 = {
-        firstName: "John",
-        lastName: "Smith",
+        firstName: 'John',
+        lastName: 'Smith',
         greeting: function (message) {
             return "".concat(message, " ").concat(this.firstName, " ").concat(this.lastName);
-        }
+        },
+        greeting2: function (message) {
+            return "".concat(message, " ").concat(this.firstName, " ").concat(this.lastName);
+        },
     };
-    console.log(user2.greeting("Goodmorning,"));
+    console.log(user2.greeting('Good morning,'));
+    console.log(user2.greeting2('Good evening,'));
+    var add = function (n1, n2) { return n1 + n2; };
+    console.log('result of addFn: ' + add(100, 300));
 }
--- Basic_TypeScript/03/interface.ts
+++ Basic_TypeScript/03/interface.ts
@@ -18,11 +18,11 @@
    */
   const greeter: (p: Person) => string = (p: Person): string => {
     return `hello, ${p.firstName} ${p.lastName}`;
-  }
+  };
 
   let user: Person = {
-    firstName: "Jane",
-    lastName: "Doe",
+    firstName: 'Jane',
+    lastName: 'Doe',
 
     // Personインタフェースにプロパティが記入されていないためエラー
     // age: 35
@@ -35,15 +35,41 @@
     firstName: string;
     lastName: string;
     greeting: (message: string) => string;
+
+    // メソッドは以下の書き方でも可
+    greeting2(message: string): string;
   }
 
   let user2: Person2 = {
-    firstName: "John",
-    lastName: "Smith",
+    firstName: 'John',
+    lastName: 'Smith',
     greeting(message) {
+      return `${message} ${this.firstName} ${this.lastName}`;
+    },
+    greeting2(message) {
       return `${message} ${this.firstName} ${this.lastName}`;
     },
   };
 
-  console.log(user2.greeting("Good morning,"));
+  console.log(user2.greeting('Good morning,'));
+  console.log(user2.greeting2('Good evening,'));
+
+  /**
+   * interfaceはオブジェクトの定義ができる
+   * → 関数宣言のもinterfaceで行うことができる
+   */
+  interface AddFn {
+    /**
+     * typeで書くと以下の通りになる。
+     *
+     * type AddFn = (a: number, b: number) => number;
+     */
+
+    // 匿名メソッドと呼ばれる
+    (a: number, b: number): number;
+  }
+
+  let add: AddFn = (n1: number, n2: number) => n1 + n2;
+
+  console.log('result of addFn: ' + add(100, 300));
 }
